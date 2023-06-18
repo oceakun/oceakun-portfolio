46e5a3f78fe441038d02ec5d6034ad610f23efff
Sagar Deep <oceakun@pop-os.localdomain>
2023-06-18 23:28:32 +0530

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,43 +18,43 @@ export const metadata: Metadata = {
     template: '%s | Sagar Deep',
   },
   description: 'Developer, novice blogger and student.',
-  openGraph: {
-    title: 'Sagar Deep',
-    description: 'Developer, novice blogger and student.',
-    url: 'https://sagardeep.io',
-    siteName: 'Sagar Deep',
-    images: [
-      {
-        url: 'https://sagardeep.io/og.jpg',
-        width: 1920,
-        height: 1080,
-      },
-    ],
-    locale: 'en-US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  twitter: {
-    title: 'Sagar Deep',
-    card: 'summary_large_image',
-  },
+  // openGraph: {
+  //   title: 'Sagar Deep',
+  //   description: 'Developer, novice blogger and student.',
+  //   url: 'https://sagardeep.io',
+  //   siteName: 'Sagar Deep',
+  //   images: [
+  //     {
+  //       url: 'https://sagardeep.io/og.jpg',
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //   ],
+  //   locale: 'en-US',
+  //   type: 'website',
+  // },
+  // robots: {
+  //   index: true,
+  //   follow: true,
+  //   googleBot: {
+  //     index: true,
+  //     follow: true,
+  //     'max-video-preview': -1,
+  //     'max-image-preview': 'large',
+  //     'max-snippet': -1,
+  //   },
+  // },
+  // twitter: {
+  //   title: 'Sagar Deep',
+  //   card: 'summary_large_image',
+  // },
   icons: {
     shortcut: './favicon.ico',
   },
-  verification: {
-    google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
-    yandex: '14d2e73487fa6c71',
-  },
+  // verification: {
+  //   google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
+  //   yandex: '14d2e73487fa6c71',
+  // },
 };
 
 export default function RootLayout({
