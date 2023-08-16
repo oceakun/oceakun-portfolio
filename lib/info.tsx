b5c3e3ccb1376ec95e50a9023dd560a178d28061
Sagar Deep <oceakun@pop-os.localdomain>
2023-08-16 23:35:36 +0530

    feat: trim the intro

--- lib/info.tsx
+++ lib/info.tsx
@@ -6,18 +6,18 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm Sagar, a <b>Frontend heavy full stack developer</b> and a <b>CS senior</b>.
+      Hey, I'm Sagar, a <b>Full stack developer</b> and a <b>CS senior</b>.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      Over the past year, I've heavily worked on the frontend with <b>React.js</b>, <b>Next.js</b> and <b>Astro</b>, with <b>Go</b> and <b>Docker</b> on the backend. <br /> <br /> 
+      {/* Over the past year, I've heavily worked on the frontend with libraries such as, <b>React.js</b>, <b>Next.js</b> and <b>Astro</b>, with <b>Go</b> and <b>Docker</b> on the backend. <br /> <br /> 
       
       I volunteer at <b>Alias</b>, a club actively maintaining an <b>Open Source Community </b>and trying to educate students about the same at our university. <br/><br /> 
-      
-      I love gardening, attending developer meetups and watching films in my free time.
+       */}
+      You'll find me gardening in my free time.
     </>
   );
 };
