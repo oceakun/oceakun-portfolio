a1297e68c3d4401626dec60c7d491c2905ba5241
Sagar Deep <sagardeep1811@hotmail.com>
2025-11-25 11:36:24 +0530

    feat: update info and restyle home page

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default async function HomePage() {
       <p className='mt-5 max-w-[460px] text-neutral-800 dark:text-neutral-200'>
         {about()}
       </p>
-      <div className='flex items-start md:items-center my-8 flex-col md:flex-row'>
+      <div className='flex items-start md:items-center my-6 flex-col md:flex-row'>
         <Image
           alt={name}
           className='grayscale-50 dark:grayscale shadow-lg shadow-rose-500/50 dark:shadow-lg dark:shadow-cyan-500/50'
@@ -68,21 +68,20 @@ export default async function HomePage() {
           priority
         />
       </div>
-      <p className='my-5 max-w-[600px] text-neutral-800 dark:text-neutral-200'>
+      {/* <hr className='my-8 max-w-[600px] border-neutral-300 dark:border-neutral-700' /> */}
+      <br />
+      <p className='max-w-[600px] text-neutral-800 dark:text-neutral-200'>
         {bio()}
       </p>
-      <hr className='my-8 max-w-[600px] border-neutral-300 dark:border-neutral-700' />
-      <p className='my-10 max-w-[600px] text-neutral-800 dark:text-neutral-200'>
+      {/* <hr className='my-8 max-w-[600px] border-neutral-300 dark:border-neutral-700' /> */}
+      <br />
+      <p className='max-w-[600px] text-neutral-800 dark:text-neutral-200'>
         {currentJob()}
       </p>
-      {/* <hr className='my-8 max-w-[600px] border-neutral-300 dark:border-neutral-700' />
-      <p className='my-10 max-w-[600px] text-neutral-800 dark:text-neutral-200'>
-        {prevJob()}
-      </p> */}
-      <hr className='my-8 max-w-[600px] border-neutral-300 dark:border-neutral-700' />
-      <p className='my-10 max-w-[600px] text-neutral-800 dark:text-neutral-200'>
+      {/* <hr className='my-8 max-w-[600px] border-neutral-300 dark:border-neutral-700' /> */}
+      {/* <p className='my-10 max-w-[600px] text-neutral-800 dark:text-neutral-200'>
         {summary()}
-      </p>
+      </p> */}
     </section>
   );
 }
